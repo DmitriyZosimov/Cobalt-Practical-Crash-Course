4908a2c836c6c34f4800b19761327cb9b2c9e79b
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2021-12-14 12:54:39 +0300

    added @EnableWebMvc

--- web/src/main/java/com/cobaltcourse/web/config/WebConfiguration.java
+++ web/src/main/java/com/cobaltcourse/web/config/WebConfiguration.java
@@ -1,17 +1,11 @@
 package com.cobaltcourse.web.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan(basePackages = "com.cobaltcourse.web")
+@EnableWebMvc
 public class WebConfiguration {
 }
